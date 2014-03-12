fdcec31ce4e4d756d296ff472f9c1217c192228c
kpavlov <kpavlov@dev.null>
2014-03-12 10:25:13 +0200

    SimpleFixSessionSettingsProvider

---
+++ core/src/main/java/fixio/netty/pipeline/client/SimpleFixSessionSettingsProvider.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright 2014 The FIX.io Project
+ *
+ * The FIX.io Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package fixio.netty.pipeline.client;
+
+/**
+ * Simple java bean FixSessionSettingsProvider implementation.
+ * <p/>
+ * <strong>Warning:</strong> This implementation is not thread-safe.
+ */
+public class SimpleFixSessionSettingsProvider implements FixSessionSettingsProvider {
+
+    private String beginString;
+    private String senderCompID;
+    private String senderSubID;
+    private String targetCompID;
+    private String targetSubID;
+    private boolean resetMsgSeqNum;
+    private int heartbeatInterval;
+
+    @Override
+    public String getSenderCompID() {
+        return senderCompID;
+    }
+
+    public void setSenderCompID(String senderCompID) {
+        this.senderCompID = senderCompID;
+    }
+
+    @Override
+    public String getSenderSubID() {
+        return senderSubID;
+    }
+
+    public void setSenderSubID(String senderSubID) {
+        this.senderSubID = senderSubID;
+    }
+
+    @Override
+    public String getTargetCompID() {
+        return targetCompID;
+    }
+
+    public void setTargetCompID(String targetCompID) {
+        this.targetCompID = targetCompID;
+    }
+
+    @Override
+    public String getTargetSubID() {
+        return targetSubID;
+    }
+
+    public void setTargetSubID(String targetSubID) {
+        this.targetSubID = targetSubID;
+    }
+
+    @Override
+    public String getBeginString() {
+        return beginString;
+    }
+
+    public void setBeginString(String beginString) {
+        this.beginString = beginString;
+    }
+
+    @Override
+    public boolean isResetMsgSeqNum() {
+        return resetMsgSeqNum;
+    }
+
+    public void setResetMsgSeqNum(boolean resetMsgSeqNum) {
+        this.resetMsgSeqNum = resetMsgSeqNum;
+    }
+
+    @Override
+    public int getHeartbeatInterval() {
+        return heartbeatInterval;
+    }
+
+    public void setHeartbeatInterval(int heartbeatInterval) {
+        this.heartbeatInterval = heartbeatInterval;
+    }
+}
---
+++ core/src/test/java/fixio/netty/pipeline/client/SimpleFixSessionSettingsProviderTest.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright 2014 The FIX.io Project
+ *
+ * The FIX.io Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package fixio.netty.pipeline.client;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.Random;
+
+import static org.apache.commons.lang3.RandomStringUtils.randomAscii;
+import static org.junit.Assert.assertEquals;
+
+public class SimpleFixSessionSettingsProviderTest {
+
+    private static SimpleFixSessionSettingsProvider settingsProvider;
+
+    private static String beginString;
+    private static String senderCompID;
+    private static String senderSubID;
+    private static String targetCompID;
+    private static String targetSubID;
+    private static boolean resetMsgSeqNum;
+    private static int heartbeatInterval;
+
+    @BeforeClass
+    public static void setUp() {
+        beginString = randomAscii(5);
+        senderCompID = randomAscii(5);
+        senderSubID = randomAscii(5);
+        targetCompID = randomAscii(5);
+        targetSubID = randomAscii(5);
+        resetMsgSeqNum = true;
+        heartbeatInterval = new Random().nextInt(100) + 1;
+
+
+        settingsProvider = new SimpleFixSessionSettingsProvider();
+        settingsProvider.setBeginString(beginString);
+        settingsProvider.setSenderCompID(senderCompID);
+        settingsProvider.setSenderSubID(senderSubID);
+        settingsProvider.setTargetCompID(targetCompID);
+        settingsProvider.setTargetSubID(targetSubID);
+        settingsProvider.setResetMsgSeqNum(resetMsgSeqNum);
+        settingsProvider.setHeartbeatInterval(heartbeatInterval);
+    }
+
+    @Test
+    public void testGetSenderCompID() throws Exception {
+        assertEquals(senderCompID, settingsProvider.getSenderCompID());
+    }
+
+    @Test
+    public void testGetSenderSubID() throws Exception {
+        assertEquals(senderSubID, settingsProvider.getSenderSubID());
+    }
+
+    @Test
+    public void testGetTargetCompID() throws Exception {
+        assertEquals(targetCompID, settingsProvider.getTargetCompID());
+    }
+
+    @Test
+    public void testGetTargetSubID() throws Exception {
+        assertEquals(targetSubID, settingsProvider.getTargetSubID());
+    }
+
+    @Test
+    public void testGetBeginString() throws Exception {
+        assertEquals(beginString, settingsProvider.getBeginString());
+    }
+
+    @Test
+    public void testIsResetMsgSeqNum() throws Exception {
+        assertEquals(resetMsgSeqNum, settingsProvider.isResetMsgSeqNum());
+    }
+
+    @Test
+    public void testGetHeartbeatInterval() throws Exception {
+        assertEquals(heartbeatInterval, settingsProvider.getHeartbeatInterval());
+    }
+}
